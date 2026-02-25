8e84c1a2a057cfa84ae855da48b36b516bb9f1d1
aiden0rchad <aiden0rchad@users.noreply.github.com>
2026-02-24 21:36:33 -0800

    fix: Bump PWA cache limit to 15MB for 9.1MB production bundle

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -31,7 +31,7 @@ export default defineConfig({
       filename: "serviceWorker.ts",
       strategies: "injectManifest",
       injectManifest: {
-        maximumFileSizeToCacheInBytes: 4000000,
+        maximumFileSizeToCacheInBytes: 15000000,
       },
       manifest: {
         name: "Stoat",
